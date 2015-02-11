89f2374835feda2a6f8a1e8bca04bb9ec40991c7
Tim Upthegrove <tim.upthegrove@gmail.com>
2015-02-10 20:42:10 -0500

    gtmp_counter appears to be working

--- barrier/gtmp_counter.c
+++ barrier/gtmp_counter.c
@@ -17,15 +17,32 @@
            repeat until sense = local_sense
 */
 
+static unsigned char sense = 0;
+static unsigned int count;
+static unsigned int my_num_threads;
 
 void gtmp_init(int num_threads){
-
+  my_num_threads = count = num_threads;
 }
 
 void gtmp_barrier(){
+  //FIXME: maybe put the local sense on its own cache line?
+  /* posix_memalign */
+  /* LEVEL1_DCACHE_LINESIZE */
+  unsigned char my_sense = sense ^ 1;
 
+  // Atomic decrement
+  if (__sync_fetch_and_sub(&count, 1) == 1) {
+    count = my_num_threads;
+    sense = my_sense;
+  } else {
+    // spin!
+    while (sense != my_sense) {
+      // do nothing
+    }
+  }
 }
 
 void gtmp_finalize(){
-
+  //FIXME: anything to do here?
 }
