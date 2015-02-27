9fa62c2d8f5e1e2812084ac5a74c485525a8db6b
Tim Upthegrove <tim.upthegrove@gmail.com>
2015-02-26 23:19:04 -0500

    Passed BVT and cleaned up code, but still perhaps some issues with high numbers of processes

--- barrier/gtmpi_dissemination.c
+++ barrier/gtmpi_dissemination.c
@@ -32,20 +32,13 @@
 	parity := 1 - parity
 */
 
-typedef struct flags_struct {
-  char *(myflags[2]);
-  //char (**partnerflags)[2];
-} flags_t;
-
-flags_t *allnodes;
-//static unsigned int vpid_next;
 static int num_procs, num_rounds;
 static char sense_init, parity_init;
 
 /**
- * Log base 2 for an integer value
+ * Ceiling of log base 2 for an integer value
  */
-static float tlog2(int val) {
+static float ceillog2(int val) {
   int i = 0;
 
   if (val > 0) {
@@ -56,74 +49,34 @@ static float tlog2(int val) {
     //we should throw an exception here
   }
 
-  return i;
-}
-
-/**
- * Ceiling function for a float value
- */
-static int tceil(float val) {
-  int intval = (int) val;
-
-  return (intval > val) ? (intval + 1) : intval; 
-}
-
-static int specialmath(int i, int k, int p) {
-  int retval = (i + (1 << k)) % p;
-  if (retval < 0) {
-    printf("Negative mod!\n");
-    fflush(stdout);
-  }
-  return retval;
+  return ((1 << i) == val) ? i : i+1;
 }
 
 void gtmpi_init(int num_threads){
-  int i, j, k;
-
   num_procs = num_threads;
-  num_rounds = tceil(tlog2(num_procs));
-
-  /* Instantiate allnodes data structure */
-  // FIXME: maybe allocate one per cache line to avoid false sharing
-  allnodes = malloc(num_procs * sizeof(flags_t));
-  
-  for (i = 0; i < num_procs; i++) {
-    for (j = 0; j < 2; j++) {
-      allnodes[i].myflags[j] = (char *) malloc(num_rounds * sizeof(char));
-      //allnodes[i].partnerflags[j] = malloc(num_rounds * sizeof(char *));
-      for (k = 0; k < num_rounds; k++) {
-        allnodes[i].myflags[j][k] = 0;
-        //allnodes[i].partnerflags[j][k] = &allnodes[specialmath(i)].myflags[j][k];
-      }
-    }
-  }
-
-  //vpid_next = 0;
+  num_rounds = ceillog2(num_procs);
   sense_init = 1;
   parity_init = 0;
 }
 
 void gtmpi_barrier(){
-  flags_t *localflags;
   int parity = parity_init;
   char sense = sense_init;
-  //unsigned int vpid = __sync_fetch_and_add(&vpid_next, 1) % num_procs;
   int vpid; 
-  int i;
+  int i, src, dst;
   MPI_Status stat;
 
   // MPI controls the IDs that we need to use
   MPI_Comm_rank(MPI_COMM_WORLD, &vpid);
-  localflags = &allnodes[vpid];
 
   for (i = 0; i < num_rounds; i++) {
     // do MPI send here
-    MPI_Send(&sense, 1, MPI_CHAR, specialmath(vpid, i, num_procs), 0, MPI_COMM_WORLD);
-    //*localflags->partnerflags[parity][i] = sense;
+    dst = (vpid + (1<<i)) % num_procs;
+    MPI_Send(&sense, 1, MPI_CHAR, dst, 0, MPI_COMM_WORLD);
 
     // do MPI recv here
-    MPI_Recv(&localflags->myflags[parity][i], 1, MPI_CHAR, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &stat);
-    //while(localflags->myflags[parity][i] == sense);
+    src = ((vpid - (1<<i)) + num_procs) % num_procs;
+    MPI_Recv(&sense, 1, MPI_CHAR, src, 0, MPI_COMM_WORLD, &stat);
   }
 
   if (parity == 1) {
@@ -134,13 +87,4 @@ void gtmpi_barrier(){
 }
 
 void gtmpi_finalize(){
-  int i, j;
-
-  for (i = 0; i < num_procs; i++) {
-    for (j = 0; j < 2; j++) {
-      free(allnodes[i].myflags[j]);
-    }
-  }
-
-  free(allnodes);
 }
