29e94dad6c3d0b51fb4012cf3c3b04edc1e78c28
Tim Upthegrove <tim.upthegrove@gmail.com>
2015-02-28 20:08:28 -0500

    Update makefile, optimize gtmpi_counter.c

--- barrier/gtmpi_counter.c
+++ barrier/gtmpi_counter.c
@@ -19,34 +19,40 @@
            repeat until sense = local_sense
 */
 
+/**
+ * Tupty changes:
+ *  1. Have one master node, allowing all other nodes to signal it
+ *  2. Have the master node signal all other nodes once it has heard from all the others
+ *        These changes allow for the O(N^2) messages to become O(N) messages
+ *  3. Allocate a single static status variable to save some space [O(1) instead of O(N) space]
+ */
 
-static MPI_Status* status_array;
 static int P;
 
 void gtmpi_init(int num_threads){
   P = num_threads;
-  status_array = (MPI_Status*) malloc((P - 1) * sizeof(MPI_Status));
 }
 
 void gtmpi_barrier(){
+  static MPI_Status status;
   int vpid, i;
 
   MPI_Comm_rank(MPI_COMM_WORLD, &vpid);
   
-  for(i = 0; i < vpid; i++)
-    MPI_Send(NULL, 0, MPI_INT, i, 1, MPI_COMM_WORLD);
-  for(i = vpid + 1; i < P; i++)
-    MPI_Send(NULL, 0, MPI_INT, i, 1, MPI_COMM_WORLD);
+  if (vpid == 0) {
+    for(i = 1; i < P; i++)
+      MPI_Recv(NULL, 0, MPI_INT, i, 1, MPI_COMM_WORLD, &status);
 
-  for(i = 0; i < vpid; i++)
-    MPI_Recv(NULL, 0, MPI_INT, i, 1, MPI_COMM_WORLD, &status_array[i]);
-  for(i = vpid + 1; i < P; i++)
-    MPI_Recv(NULL, 0, MPI_INT, i, 1, MPI_COMM_WORLD, &status_array[i-1]);
+    for(i = 1; i < P; i++)
+      MPI_Send(NULL, 0, MPI_INT, i, 1, MPI_COMM_WORLD);
+    
+  } else {
+    MPI_Send(NULL, 0, MPI_INT, 0, 1, MPI_COMM_WORLD);
+    MPI_Recv(NULL, 0, MPI_INT, 0, 1, MPI_COMM_WORLD, &status);
+  }
+
 }
 
 void gtmpi_finalize(){
-  if(status_array != NULL){
-    free(status_array);
-  }
 }
 
