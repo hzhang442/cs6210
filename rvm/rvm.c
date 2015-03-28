597893a6dfef5e3ad5fa097d1c17c8242fa87520
Tim Upthegrove <tim.upthegrove@gmail.com>
2015-03-28 18:21:44 -0400

    Start implementation, save to checkpoint

--- rvm/rvm.c
+++ rvm/rvm.c
@@ -1,16 +1,79 @@
 #include"rvm.h"
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+#define PATH_BUF_SIZE       (128)
+#define SEGNAME_SIZE        (128)
+#define REDO_PATH_BUF_SIZE  (PATH_BUF_SIZE + 16)
+
+static rvm_t rvm;
+
+int keyeq(seqsrchst_key a, seqsrchst_key b) {
+  return strcmp((char *) a, (char *) b);
+}
+
 /*
   Initialize the library with the specified directory as backing store.
 */
 rvm_t rvm_init(const char *directory){
+  char redopath[REDO_PATH_BUF_SIZE];
+  FILE *f;
+  struct stat st = {0};
 
+  /* Only make the directory if it does not exist */
+  if (stat(directory, &st) == -1) {
+    mkdir(directory, 0744);
+  }
+
+  strncpy(rvm->prefix, directory, (PATH_BUF_SIZE - 1));
+
+  /* Initialize data structures too */
+  seqsrchst_init(&(rvm->segst), keyeq);
+
+  /* Create redo log file */
+  strcpy(redopath, rvm->prefix);
+  strcpy(&(redopath[strlen(redopath)]), "/redo.log");
+  f = fopen(redopath, "w+");
+  rvm->redofd = fileno(f);
 }
+
 /*
   map a segment from disk into memory. If the segment does not already exist, then create it and give it size size_to_create. If the segment exists but is shorter than size_to_create, then extend it until it is long enough. It is an error to try to map the same segment twice.
 */
 void *rvm_map(rvm_t rvm, const char *segname, int size_to_create){
+  segment_t seg;
 
+  /* Check if segment exists by name */
+  if (seqsrchst_contains(&(rvm->segst), (seqsrchst_key) segname)) {
+    seg = (segment_t) seqsrchst_get(&(rvm->segst), (seqsrchst_key) segname);
+
+    /* Check if the segment size is too small */
+    if (seg->size < size_to_create) {
+      /* Realloc */
+      if ((seg->segbase = realloc(seg->segbase, size_to_create)) == NULL) {
+        /* We failed, so return error */
+        return (void *) -1;
+      } 
+    }
+  } else {
+    seg = malloc(sizeof(struct _segment_t));
+    strncpy(seg->segname, segname, SEGNAME_SIZE-1);
+    seg->size = size_to_create;
+
+    /* If no, malloc memory, create log file, and put into data struct */
+    if ((seg->segbase = malloc(size_to_create)) == NULL) {
+      /* We failed, so return error */
+      return (void *) -1;
+    } 
+  }
+
+  /* FIXME: make sure that this is the right thing to return */
+  return seg->segbase;
 }
 
 /*
@@ -18,6 +81,12 @@ void *rvm_map(rvm_t rvm, const char *segname, int size_to_create){
 */
 void rvm_unmap(rvm_t rvm, void *segbase){
 
+  /* FIXME: are these the steps for destroy? */
+  /* Look up memory by segbase in data structure */
+  /* Remove that instance */
+  /* Do linux unmap on log */
+  /* Clean up log file handle */
+  /* Free memory */
 }
 
 /*
